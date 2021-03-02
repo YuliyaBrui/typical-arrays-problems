f4779d8d73ac65793e8a2969eede48a320e175f9
YuliyaBrui <juljadivina@gmail.com>
2021-03-02 10:39:57 +0300

    feat:add desicion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (arr) {
+ if((Array.isArray(arr)) && (arr.length>0))
+ {arr.sort((a,b)=>a-b);
+    return arr[0];
+    }else{return 0}}
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max (arr) { 
+  if((Array.isArray(arr)) && (arr.length>0))
+ { arr.sort((a,b)=> b-a);
+  return arr[0];}else{return 0}}
 
-exports.avg = function avg (array) {
-  return 0;
-}
+
+exports.avg = function avg (arr) { let result=0;
+  if((Array.isArray(arr)) && (arr.length>0)){
+    for (let i = 0; i < arr.length; i++) {
+     result += arr[i];}
+   let x= result/arr.length;
+  return(x);}else{return 0}}
